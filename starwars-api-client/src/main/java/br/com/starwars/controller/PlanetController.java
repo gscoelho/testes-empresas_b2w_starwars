e428cd35af5bb26d6f3a732ab38bb432a842deee
gustavo.coelho <gustavo.coelho@4ward.com.br>
2019-03-14 21:04:51 -0300

    comment on controller for author and date descriptions

--- starwars-api-client/src/main/java/br/com/starwars/controller/PlanetController.java
+++ starwars-api-client/src/main/java/br/com/starwars/controller/PlanetController.java
@@ -12,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.starwars.business.PlanetBusiness;
 import br.com.starwars.entitys.PlanetEntity;
 
+/**
+ * @author: Gustavo Silva Coelho
+ * @date: 15/03/2019 
+ */
+
 @RestController
 @RequestMapping("/planets")
 public class PlanetController {
